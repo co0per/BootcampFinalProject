6a3e1959d30f44356b2917fa9f436cebeab34881
Gerardo <alias_gerardo@yahoo.com.ar>
2018-03-08 20:37:17 -0300

    v0.1: Video player and info operative.

--- my-app/src/components/app.js
+++ my-app/src/components/app.js
@@ -1,12 +1,39 @@
+/*****************************************************************************
+Renders the app!
+*****************************************************************************/
+
 import React from 'react'
+import VideoArea from './video_area.js'
+import {Video} from '../lib/video_utils.js'
+
+//Constants:
+const appBodyClass = "app-body";
+const testVideo = new Video("Hasta acá nos ayudo dios!",
+                            "IKkTRwM_5ao",
+                            "Las pastillas del abuelo - desafios - Hasta aca nos ayudo dios!")
+
 
 export default class App extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      /*TODO: Will playing lists be available? */
+      playingList: [],
+      playingVideo: testVideo
+    }
+  }
 
   //Insert main containers here!
   render() {
     return (
-      <div className="app-body">
-        <p> Hello World! </p>
+      <div className={appBodyClass}>
+        <VideoArea
+          title={this.state.playingVideo.title}
+          description={this.state.playingVideo.description}
+          videoId={this.state.playingVideo.id}
+          defaultHeight="360"
+          defaultWidth="640" />
       </div>
     )
   }
